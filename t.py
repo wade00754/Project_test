7ada5a2dccd86741cf040e983295ebd6663f456c
wade00754 <wade00754@gmail.com>
2024-01-24 11:14:40 +0800

    update t.py

--- t.py
+++ t.py
@@ -1,7 +1,6 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("yolov8n.yaml")  # build a new model from scratch
 model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
 
 if __name__ == "__main__":
