3da1813b6e7675f1e114764b647d6994113f3138
wade00754 <wade00754@gmail.com>
2024-01-24 11:00:53 +0800

    first commit

---
+++ t.py
@@ -0,0 +1,12 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8n.yaml")  # build a new model from scratch
+model = YOLO("yolov8n.pt")  # load a pretrained model (recommended for training)
+
+if __name__ == "__main__":
+    # Use the model
+    results = model.train(data="coco128.yaml", epochs=3)  # train the model
+    results = model.val()  # evaluate model performance on the validation set
+    results = model("https://ultralytics.com/images/bus.jpg")  # predict on an image
+    success = YOLO("yolov8n.pt").export(format="onnx")  # export a model to ONNX format
