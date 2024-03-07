9e10fdba928c5f9faea442f3ebe7476b9fb8e35c
wade00754 <wade00754@gmail.com>
2024-03-08 03:38:42 +0800

    week3

--- test2.py
+++ test2.py
@@ -1,7 +1,9 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO("runs/detect/train/weights/best.pt")
+model = YOLO("yolov8n.pt")
+# model = YOLO("runs/detect/train/weights/best.pt")
 
 if __name__ == "__main__":
-    model.predict(source="images", save=True)  # predict on an image
+    result = model.predict(source="0", show=True, save=True)  # predict on an image
+    # print(result)
